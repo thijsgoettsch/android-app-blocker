c0341192ce3d11ee3a2e666217b85e4572bee3b8
Thijs Goettsch <thijs.goettsch@gmail.com>
2025-06-09 22:39:00 +0200

    Setup monitoring

--- app/src/main/java/io/github/appblocker/MainActivity.kt
+++ app/src/main/java/io/github/appblocker/MainActivity.kt
@@ -1,11 +1,16 @@
 package io.github.appblocker
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +18,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Log.d("MainActivity", "onCreate: started")
         setContentView(R.layout.activity_main)
+
     }
+
+
+
 }
