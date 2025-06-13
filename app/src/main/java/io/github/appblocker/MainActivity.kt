bd1f36059ac696c7949bd6ffd472db164ca607fe
Thijs Goettsch <thijs.goettsch@gmail.com>
2025-06-13 23:32:00 +0200

    new everything

--- app/src/main/java/io/github/appblocker/MainActivity.kt
+++ app/src/main/java/io/github/appblocker/MainActivity.kt
@@ -1,26 +1,47 @@
 package io.github.appblocker
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.appblocker.ui.theme.AppBlockerTheme
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("MainActivity", "onCreate: started")
-        setContentView(R.layout.activity_main)
-
+        enableEdgeToEdge()
+        setContent {
+            AppBlockerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
     }
+}
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
-
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppBlockerTheme {
+        Greeting("Android")
+    }
 }
